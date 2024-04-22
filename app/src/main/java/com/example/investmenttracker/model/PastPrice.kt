b6ac905ed94f1e43086235215aed05af64af2bad
michaelallan <mb.allan@ymail.com>
2024-04-21 21:52:07 -0700

    Added createdAt and updatedAt properties to the DatabaseEntry class; Modified the Portfolio's getPriceAt method to take currency conversion into account

--- app/src/main/java/com/example/investmenttracker/model/PastPrice.kt
+++ app/src/main/java/com/example/investmenttracker/model/PastPrice.kt
@@ -1,9 +1,9 @@
 package com.example.investmenttracker.model
 
 /**
- * The price of an investment vehicle at a specific date and time
+ * The price, in USD, of an investment vehicle at a specific date and time
  *
  * @param dateTime The date and time at which an investment had the given rate
- * @param price The price of an investment at the given date
+ * @param price The price, in USD, of an investment at the given date
  */
 data class PastPrice(val dateTime: DateTime, val price: Float)
